766fc21dcdee1e353dbe4d668457a38cc09fea54
Tyler Smith <tylersmith@jhu.edu>
2023-04-16 13:37:30 -0700

    added BMI, MUAC to Table 1

--- 03A_table1.R
+++ 03A_table1.R
@@ -8,5 +8,30 @@
 ##### Preliminaries ############################################################
 # Load Packages
 library(tidyverse)
+library(table1)
 
 ##### Generate Table ###########################################################
+# Labels
+label(df_covar$AGE)       <- "Age"
+label(df_covar$SEGSTAGE)  <- "Gestational Age"
+label(df_covar$PARITY)    <- "Parity"
+label(df_covar$EDUCATION) <- "Education"
+label(df_covar$LSI)       <- "Living Standard Index"
+label(df_covar$SEBMI)     <- "Body Mass Index"
+label(df_covar$medSEMUAC) <- "Mid-upper Arm Circumference"
+label(df_covar$PESTICIDE) <- "Pesticide User"
+label(df_covar$PETOBAC)   <- "Chewing Tobacco User"
+label(df_covar$PEBETEL)   <- "Betel Nut User"
+label(df_covar$PEHCIGAR)  <- "Husband Smokes at Home"
+
+# Units
+units(df_covar$AGE)       <- "years"
+units(df_covar$SEGSTAGE)  <- "weeks"
+units(df_covar$SEBMI)     <- "kg/m^2"
+units(df_covar$medSEMUAC) <- "cm"
+
+# Table
+(tbl1 <- table1(~ AGE + SEGSTAGE + PARITY + EDUCATION + LSI + SEBMI + medSEMUAC + PESTICIDE + PETOBAC + PEBETEL + PEHCIGAR | wAs10, 
+  data = df_covar, overall = FALSE, render.continuous = c("Mean (SD)" = "MEAN (SD)"), extra.col = list(`p` = pval_tbl1)))
+  
+  
