a362beccec6a043b1d7906e0b6fb0011a5fcd843
Tyler Smith <tylersmith@jhu.edu>
2023-04-17 18:10:32 -0700

    update summary statistics for elements

--- 03B_table2.R
+++ 03B_table2.R
@@ -2,6 +2,8 @@
 # Pregnancy, Arsenic, and Immune Response (PAIR) Study
 # Identify Element Mixtures -- Table 2
 
+# Note: This script currently uses the first imputation as an example.
+
 # Tyler Smith
 # April 4, 2023
 
@@ -10,7 +12,25 @@
 library(tidyverse)
 
 ##### Generate Table ###########################################################
-tbl2_water_lod <- df_water_llod_ind %>% 
-  summary_table(filter = c("Cd","Cu","Pb","Zn"))
+# Section: LLOD
+(tbl2_water_lod_val <- df_water_llod_val %>%
+  filter(!ELEMENT %in% c("Cd","Cu","K","Mg","Na","Pb","Zn")) %>%
+  mutate(LLOD = ifelse(LLOD >= 1, round(LLOD, 1), signif(LLOD, 1))) %>%
+  select(Element = ELEMENT, LLOD) %>%
+  arrange(Element))
 
-#(tbl2_water <- left_join(tblS1_water_lod, tblS1_water_val, by = "Element"))
+# Section: Values <LLOD
+(tbl2_water_lod_np <- df_water_llod_ind %>% 
+  summary_table_lod(filter = c("Cd","Cu","Pb","Zn")))
+
+# Section: GM, GSD, and Percentiles
+(tbl2_water_val <- df_water_impt %>%
+  filter(.imp == 1) %>%
+  summary_table_val())
+
+# Join Table Sections
+tbl2_water <- left_join(tbl2_water_lod_val, tbl2_water_lod_np, by = "Element")
+tbl2_water <- left_join(tbl2_water, tbl2_water_val, by = "Element")
+
+# Review Table
+tbl2_water %>% head()
--- 03C_table3.R
+++ 03C_table3.R
@@ -2,6 +2,8 @@
 # Pregnancy, Arsenic, and Immune Response (PAIR) Study
 # Identify Element Mixtures -- Table 3
 
+# Note: This script currently uses the first imputation as an example.
+
 # Tyler Smith
 # April 4, 2023
 
@@ -10,3 +12,34 @@
 library(tidyverse)
 
 ##### Generate Table ###########################################################
+# Section: LLOD
+(tbl3_urine_lod_val <- df_urine_llod_val %>%
+  filter(!ELEMENT %in% c("U")) %>%
+  mutate(LLOD = ifelse(LLOD >= 1, round(LLOD, 1), signif(LLOD, 1))) %>%
+  select(Element = ELEMENT, LLOD) %>%
+  arrange(Element))
+
+# Section: Values <LLOD
+(tbl3_urine_lod_np <- df_urine_llod_ind %>% 
+  summary_table_lod(filter = "U"))
+
+# Section: GM, GSD, and Percentiles
+(tbl3_urine_val <- df_urine_impt %>%
+  filter(.imp == 1) %>%
+  select(-c(.imp,.id,SPECIFICGRAVITY)) %>%
+  summary_table_val())
+
+# Join Table Sections
+tbl3_urine <- left_join(tbl3_urine_lod_val, tbl3_urine_lod_np, by = "Element")
+tbl3_urine <- left_join(tbl3_urine, tbl3_urine_val, by = "Element")
+
+# Set Arsenic to Missing
+# (Note: Urinary arsenic is the sum of urinary inorganic, monomethyl, and dimethyl
+#  arsenic. The LLOD for each species was 0.05 µg/L. Values of all three species
+#  were ≥LLOD for all participants.)
+tbl3_urine <- tbl3_urine %>%
+  mutate(LLOD = ifelse(Element == "As", NA, LLOD)) %>%
+  mutate(`<LLOD [n (%)]` = ifelse(Element == "As", "0 (0)", `<LLOD [n (%)]`))
+
+# Review Table
+tbl3_urine %>% head()
